2b48dd2da18bd8b2fbf45aa01ffc65a926b5cb5e
Krishna Kant Tiwari <143533029+krishnakant-tiwari@users.noreply.github.com>
2023-09-25 21:59:34 +0530

    printing name in a single line
    Printing a name using ascii values / pattern printing in a single line.

---
+++ nameInSingleLine.c
@@ -0,0 +1,123 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,j;
+
+    for( i=1; i<=5; i++)
+    {
+        for( j=1; j<=5; j++)
+        {
+        if( i==j && j!=2 || i+j==6 && j!=2 || j==1 )
+        printf("%c",3);
+        else
+        printf(" ");
+        }
+        printf("  ");
+    
+
+        for( j=1; j<=5; j++)
+        {
+        if( i==1 || i==3 || i==j && j!=2 || j==1 || j==5 && i!=4 && i!=5 )
+        printf("%c",4);
+        else
+        printf(" ");
+        }
+        printf("  ");
+    
+     
+        for( j=1; j<=5; j++)
+        {
+        if( i==1 || i==5 || j==3 )
+        printf("%c",5);
+        else
+        printf(" ");
+        }
+        printf("  ");
+    
+
+        for( j=1; j<=5; j++)
+        {
+        if( i==1 || i==3 || i==5 || j==1 && i!=4 || j==5 && i!=2 )
+        printf("%c",6);
+        else
+        printf(" ");
+        }
+        printf("  ");
+    
+    
+        for( j=1; j<=5; j++)
+        {
+        if( i==3 || j==1 || j==5)
+        printf("%c",17);
+        else
+        printf(" ");
+        }
+        printf("  ");
+    
+
+        for( j=1; j<=5; j++)
+        {
+        if( i==j || j==1 || j==5)
+        printf("%c",2);
+        else
+        printf(" ");
+        }
+        printf("  ");
+    
+
+        for( j=1; j<=5; j++)
+        {
+        if( i==1 || i==3 || j==1 || j==5 )
+        printf("%c",14);
+        else
+        printf(" ");
+        }
+        printf("     ");
+
+
+        for( j=1; j<=5; j++)
+        {
+        if( i==j && j!=2 || i+j==6 && j!=2 || j==1 )
+        printf("%c",5);
+        else
+        printf(" ");
+        }
+        printf("  ");
+
+
+        for( j=1; j<=5; j++)
+        {
+        if( i==1 || i==3 || j==1 || j==5 )
+        printf("%c",6);
+        else
+        printf(" ");
+        }
+        printf("  ");
+
+
+        for( j=1; j<=5; j++)
+        {
+        if( i==j || j==1 || j==5)
+        printf("%c",15);
+        else
+        printf(" ");
+        }
+        printf("  ");
+
+
+        for( j=1; j<=5; j++)
+        {
+        if( i==1 || j==3)
+        printf("%c",16);
+        else
+        printf(" ");
+        }
+        printf("  ");
+        
+    printf("\n");
+
+    }
+
+    return 0;
+}
