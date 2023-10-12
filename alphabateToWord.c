476e0eeab5c527d7151da9c1180ea19d2932886c
Krishna Kant Tiwari <143533029+krishnakant-tiwari@users.noreply.github.com>
2023-10-12 22:54:06 +0530

    Create alphabateToWord.c
    a character from alphabates input given by the user wll be converted to word.

---
+++ alphabateToWord.c
@@ -0,0 +1,117 @@
+#include<stdio.h>
+int main() 
+{
+    char ch;
+    printf("Enter the character : ");
+    scanf("%c",&ch);
+
+    switch(ch)
+    {
+        case 'A' :
+        case 'a' : printf("\nApple");
+        break;
+
+        case 'B' :
+        case 'b' : printf("\nBat");
+        break;
+
+        case 'C' :
+        case 'c' : printf("\nCat");
+        break;
+
+        case 'D' :
+        case 'd' : printf("\nDog");
+        break;
+  
+        case 'E' :
+        case 'e' : printf("\nElephant");
+        break;
+
+        case 'F' :
+        case 'f' : printf("\nFish");
+        break;
+
+        case 'G' :
+        case 'g' : printf("\nGrapes");
+        break;
+
+        case 'H' :
+        case 'h' : printf("\nHorse");
+        break;
+
+        case 'I' :
+        case 'i' : printf("\nInkpot");
+        break;
+
+        case 'J' :
+        case 'j' : printf("\nJug");
+        break;
+
+        case 'K' :
+        case 'k' : printf("\nKite");
+        break;
+
+        case 'L' :
+        case 'l' : printf("\nLion");
+        break;
+
+        case 'M' :
+        case 'm' : printf("\nMonkey");
+        break;
+
+        case 'N' :
+        case 'n' : printf("\nNext");
+        break;
+
+        case 'O' :
+        case 'o' : printf("\nOrange");
+        break;
+
+        case 'P' :
+        case 'p' : printf("\nPeacock");
+        break;
+
+        case 'Q' :
+        case 'q' : printf("\nQueen");
+        break;
+
+        case 'R' :
+        case 'r' : printf("\nRose");
+        break;
+
+        case 'S' :
+        case 's' : printf("\nSun");
+        break;
+
+        case 'T' :
+        case 't' : printf("\nTiger");
+        break;
+
+        case 'U' :
+        case 'u' : printf("\nUmbrella");
+        break;
+
+        case 'V' :
+        case 'v' : printf("\nVan");
+        break;
+
+        case 'W' :
+        case 'w' : printf("\nWatch");
+        break;
+
+        case 'X' :
+        case 'x' : printf("\nX-rey");
+        break;
+
+        case 'Y' :
+        case 'y' : printf("\nYoyo");
+        break;
+
+        case 'Z' :
+        case 'z' : printf("\nZebra");
+        break;
+    }
+
+    return 0;
+
+}
