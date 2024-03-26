56b9c732c4e4d0886a70588dd38e87ec64c638a8
Krishna Kant Tiwari <143533029+krishnakant-tiwari@users.noreply.github.com>
2024-03-26 19:58:50 +0530

    Update getArray.c

--- array/getArray.c
+++ array/getArray.c
@@ -4,16 +4,18 @@ int main() {
 
     int arr[5];
 
-    printf("Enter the array elements : ");
-    
+    printf("Enter the 5 array elements : ");
+
+    // taking input from user
     for(int i=0; i<5; i++)
     {
         scanf("%d",&arr[i]);
     }
 
+    // printing array elements 
     for(int i=0; i<5; i++)
     {
-        printf("\n arr[%d] = %d",i,arr[i]);
+        printf("\n arr[%d] = %d",i+1,arr[i]);
     }
 
     return 0;
